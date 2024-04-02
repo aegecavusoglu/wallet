7e89201be80aa6721016b2f07fbbb37aec128809
Ali Ege Çavuşoğlu <114364190+aliegecavusoglu@users.noreply.github.com>
2024-04-03 00:24:42 +0300

    Initial commit

---
+++ src/main/java/com/example/cuzdan/CuzdanApplication.java
@@ -0,0 +1,13 @@
+package com.example.cuzdan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuzdanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuzdanApplication.class, args);
+    }
+
+}
